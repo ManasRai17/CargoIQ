dad692bded9fbe6c59e52f19c69a54747990ada5
ananyakhandelwal <ananyaakkhandelwal562@gmail.com>
2026-03-17 00:04:09 +0530

    Simplify App component to display header only
    Removed routing and navigation components, replaced with a simple header.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,5 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-
-import Dashboard from "./pages/Dashboard"
-import AddOrder from "./pages/AddOrder"
-import RoutesPage from "./pages/RoutesPage"
-
 function App() {
-  return (
-    <BrowserRouter>
-
-      <nav style={{ padding: "10px", background: "#eee" }}>
-        <Link to="/" style={{ marginRight: "15px" }}>Dashboard</Link>
-        <Link to="/add-order" style={{ marginRight: "15px" }}>Add Order</Link>
-        <Link to="/routes">Routes</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/add-order" element={<AddOrder />} />
-        <Route path="/routes" element={<RoutesPage />} />
-      </Routes>
-
-    </BrowserRouter>
-  )
+  return <h1>CARGOIQ APP RUNNING</h1>
 }
 
 export default App
